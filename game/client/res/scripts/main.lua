1721257726d92f449c7214c3bcd976baee286b87
zhoubao <bob.chow@163.com>
2015-06-09 12:16:25 +0800

    test

--- game/client/res/scripts/main.lua
+++ game/client/res/scripts/main.lua
@@ -3,9 +3,9 @@ package.path = "scripts/?.lua;scripts/lib/?.lua"
 
 function init()
 	print("hello freegame")
-	print("_G")
-	print(freeg)
-	print(freeg.RootScene)
+	print("_G", _G, #_G)
+	print("freeg", freeg, #freeg)
+	print("freeg.RootScene", freeg.RootScene)
 
 	local root = freeg.RootScene:create()
 	print(tolua.type(root))
