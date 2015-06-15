192c1fa436b35cf694dccf6d9b52fa67ef27f8c4
zhoubao <bob.chow@163.com>
2015-06-15 19:03:38 +0800

    add lua-bindings

---
+++ src/freegame.h
@@ -0,0 +1,12 @@
+#ifndef H_FREEGAME_H
+#define H_FREEGAME_H
+
+#include "core/action.h"
+#include "core/camera.h"
+#include "core/core.h"
+#include "core/mesh_node.h"
+#include "core/scene_node.h"
+#include "core/transform.h"
+#include "core/world.h"
+
+#endif //H_FREEGAME_H
